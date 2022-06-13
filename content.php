e3fda4fce1991c8a47a7c8bcae868d74902d23d7
h-wholf <kemalmoto.e5@gmail.com>
2022-06-13 14:47:44 -0500

    pagina de inicio

--- content.php
+++ content.php
@@ -1,58 +1,22 @@
-<!--
-<body>
-  <h1><center><a class="boton1" style="cursor: pointer" onClick="mostrar_ocultar('detalles2')"   >ocultar funcion</a></center></h1>
-<div class="detalles" id="ocultar" >
-
-
-
-</div>
-
 <div>
-<h1 class="detalles2" id="detalles2">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consectetur voluptatum, reiciendis 
-  cum consequuntur totam aut explicabo necessitatibus placeat labore incidunt voluptate,
-   laudantium neque ipsum suscipit maxime dolorem atque ut.</h1>
-
-
-
+    <center><a class="titulo" >presidencia auxiliar de santa maria guadalupe tecola</a></center>
 </div>
 
-
-
-</body>
--->
-<header>
-<div class="titulo"><center>PRESISDENCIA AUXILIAR SANTA MARIA GUADALUPE TECOLA</center></div>
-<br><br><br>
-
-</header>
-<div class="contenedor">
+<form action="index2.php" method="post">
+<div class="contenido">
 	
-<section class="info">
-
-<ul>
-	<li style="--i:5;--clr:#1877f2"><a href="#"><span><i class="fa-solid fa-person-chalkboard"></i></span> nueva imagen</a></li>
-	<li style="--i:4;--clr:#25d366"><a href="#"><span><i class="fa-solid fa-circle-info"></i></span> informacion</a></li>
-	<li style="--i:3;--clr:#1da1f2"><a href="#"><span><i class="fa-solid fa-pen-nib"></i></span> registro civil</a></li>
-	<li style="--i:2;--clr:#c32aa3"><a href="#"><span><i class="fa-regular fa-clock"></i></span> horarios</a></li>
-	<li style="--i:1;--clr:#c32aa3"><a href="https://www.google.com"><span><i class="fa-solid fa-person-digging"></i></span>obras</a></li>
-
-
-
-</ul>
-<br>
-
-<ul>
-	<li style="--i:5;--clr:#4FDA00"><a href="#"><span><i class="fa-solid fa-person-chalkboard"></i></span> gabinete</a></li>
-	<li style="--i:4;--clr:#DA00CC"><a href="#"><span><i class="fa-thin fa-d"></i></span> dif municipal</a></li>
-	<li style="--i:3;--clr:#1da1f2"><a href="#"><span><i class="fa-solid fa-file-pen"></i></span> tramites</a></li>
-	<li style="--i:2;--clr:#40DDED"><a href="#"><span><i class="fa-solid fa-book-medical"></i></span> salud</a></li>
-	<li style="--i:1;--clr:#AAC02F"><a href="#"><span><i class="fa-solid fa-book-open-reader"></i></span> educacion</a></li>
-
-
-
-</ul>
-
-</section>
-
+<div class="inputBox">
+	<input id="nombre" type="tetx" required="required">
+	<span>Nombre usuario</span></div>
+<div class="inputBox">
+	<input id="pass" type="password" required="required">
+	<span>Contraseña</span>
 </div>
+<div >
+	<button class="boton" type="submit">ACCEDER</button>
+</div>
+</div>
+</form>
+<div class="fondo">
+
+	</div>
