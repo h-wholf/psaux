dcceebfbda4426c759d4e28d5869b26ae6ed2c75
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-20 09:46:59 -0500

    Update consultas.php

--- consultas.php
+++ consultas.php
@@ -2,7 +2,7 @@
 
 $db = new SQLite3('libros_114_10.db');
 $statement = $db->prepare('SELECT * FROM acta WHERE nombre = :"MARIA";');
-$statement->bindValue(':id', $id);
+$statement->bindValue(':MARIA', $nombre);
 
 $result = $statement->execute();
 
