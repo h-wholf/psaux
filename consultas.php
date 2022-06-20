eb5f8d172521f577c92352a183916b65d6cfa22d
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-20 09:53:38 -0500

    Update consultas.php

--- consultas.php
+++ consultas.php
@@ -1,6 +1,6 @@
 <?php
 $bd = new SQLite3('libros_114_10.db');
 
-//var_dump($bd->querySingle('SELECT nombre FROM acta WHERE nombre="MARIA"'));
+var_dump($bd->querySingle('SELECT nombre FROM acta WHERE nombre="MARIA"'));
 print_r($bd->querySingle('SELECT nombre FROM acta WHERE nombre="MARIA"', true));
 ?>
