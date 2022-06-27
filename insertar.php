dbb9e0ed75feb8791272bd08780c3123184d60d7
h-wholf <kemalmoto.e5@gmail.com>
2022-06-27 15:04:07 -0500

    joal

--- insertar.php
+++ insertar.php
@@ -33,7 +33,12 @@ $n_acta = ($_POST["n_acta"]);
 
 $consulta = shell_exec("echo 'select * from acta where año = $año and n_acta = $n_acta;'|sqlite3 libros_114_10.db");
 
-echo "esta acta pertenece a  ". $consulta
+echo "esta acta pertenece a  ". $consulta;
+
+if ($consulta = "" ) {
+
+	echo "no hay datos puedes continuar a registrar";
+}
 
 
 
